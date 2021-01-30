4ad4934d4a8aa98400991c6440dae0f2e9a56077
ququzone <xueping.yang@gmail.com>
2021-01-31 00:19:12 +0800

    add ERC20Mock contract

---
+++ contracts/mocks/ERC20Mock.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.6.0 <0.8.0;
+
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// mock class using ERC20
+contract ERC20Mock is ERC20 {
+    constructor (
+        string memory name,
+        string memory symbol,
+        uint8 decimals,
+        uint256 initialBalance
+    ) public payable ERC20(name, symbol) {
+        console.log("Deploying a ERC20:", name);
+        _setupDecimals(decimals);
+        _mint(msg.sender, initialBalance);
+    }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
+
+    function transferInternal(address from, address to, uint256 value) public {
+        _transfer(from, to, value);
+    }
+
+    function approveInternal(address owner, address spender, uint256 value) public {
+        _approve(owner, spender, value);
+    }
+}
