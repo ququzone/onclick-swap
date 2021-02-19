b0f02bf297c0d2a8fde3969d2db1b66d44d2c9fe
ququzone <xueping.yang@gmail.com>
2021-02-19 12:25:02 +0800

    update kovan contract info

--- scripts/swap.js
+++ scripts/swap.js
@@ -7,9 +7,11 @@ async function main() {
 
     const overrides = {
         value: '10000000000',
+        gasLimit: 1000000,
+        gasPrice: '1000000000',
     };
 
-    await swap.swap('0x79ADC6A4b41C5dc4E429094B387f133cbE89f174', '10000000000', '69690060800000', [
+    await swap.swap('0xd3aaa7e009d2982164e82b855d0ce87c7dd364db', '10000000000', '69690', [
         '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
         '0x55111f7CB96f554746b6aE13270905792C242E7B'
     ], '0x05262B75ae78c26E6008608681B6bd1bF391DF5B', 9999999999, overrides);
