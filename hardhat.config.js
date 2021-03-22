1f5a5738aa7df6fa23c0c5438776d3e644e93f86
ququzone <xueping.yang@gmail.com>
2021-03-22 03:54:37 +0000

    update bsc testnet contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -42,6 +42,18 @@ module.exports = {
       accounts: [`0x${process.env.PRIVATE_KEY}`],
       throwOnTransactionFailures: true,
       throwOnCallFailures: false
+    },
+    bsc: {
+      url: `https://bsc-dataseed.binance.org`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      throwOnTransactionFailures: true,
+      throwOnCallFailures: false
+    },
+    bsc_test: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      throwOnTransactionFailures: true,
+      throwOnCallFailures: false
     }
   },
   etherscan: {
